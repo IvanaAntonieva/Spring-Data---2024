f7919cf4477e22b69aa6db2d947438051ab56430
Ivana Vasileva <106319705+IvanaAntonieva@users.noreply.github.com>
2024-03-25 23:58:24 +0200

    Add files via upload

---
+++ Spring Data Intro/BookshopSystem/src/main/java/org/bookshopsystem/service/BookService.java
@@ -0,0 +1,14 @@
+package org.bookshopsystem.service;
+
+import java.io.IOException;
+import java.util.*;
+
+public interface BookService {
+    void seedBooks() throws IOException;
+
+    List<String> findAllBooksAfterYear2000();
+
+    List<String> findAllByBookByGeorgePowellOrdered();
+
+    void createBook();
+}
