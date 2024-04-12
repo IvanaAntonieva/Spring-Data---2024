cd72ba6811a6380fdb1716674332933f8e55aeef
Ivana Vasileva <106319705+IvanaAntonieva@users.noreply.github.com>
2024-04-12 20:23:26 +0300

    Delete src directory

--- src/main/java/org/cardealer/data/repositories/PartRepository.java
+++
@@ -1,9 +0,0 @@
-package org.cardealer.data.repositories;
-
-import org.cardealer.data.entities.Part;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartRepository extends JpaRepository<Part, Long> {
-}
--- src/main/java/org/cardealer/service/PartService.java
+++
@@ -1,8 +0,0 @@
-package org.cardealer.service;
-
-import jakarta.xml.bind.JAXBException;
-
-public interface PartService {
-
-    void seedParts() throws JAXBException;
-}
